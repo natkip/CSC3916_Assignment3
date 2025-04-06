70e9fb3387aa4039de4f1b273c056ef93e68ea17
natannekip <natashakippur@gmail.com>
2025-04-06 15:39:09 -0600

    movies

--- Movies.js
+++ Movies.js
@@ -1,38 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB);
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit the process if the connection fails (optional)
-  }
-};
-
-connectDB();
-
-// Movie schema
-<<<<<<< HEAD
 const MovieSchema = new Schema({
-  title: { type: String, required: true, index: true},
-  releaseDate: {type: Number, min: [1900, 'Must be greater than 1899'], max: {2100, 'Must be less than 2100'}},
-=======
-var MovieSchema = new Schema({
-  title: { type: String, required: true, index: true },
-  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
->>>>>>> origin/main
-  genre: {
-    type: String,
-    enum: [
-      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
-    ],
-  },
-  actors: [{
-    actorName: String,
-    characterName: String,
-  }],
+    title: { type: String, required: true, index: true },
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100'] },
+    genre: { 
+        type: String, 
+        enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'] 
+    },
+    actors: [{
+        actorName: { type: String, required: true },
+        characterName: { type: String, required: true }
+    }],
 });
 
 module.exports = mongoose.model('Movie', MovieSchema);
